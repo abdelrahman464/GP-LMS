d93f78b5077ebdd68801e349a7f4bbfb32dcac58
Abd_El Rahman Mohamed <apdomedo6@gmail.com>
2023-12-05 13:47:30 +0200

    adding group chat event to scket server

--- socket/socketio-server.js
+++ socket/socketio-server.js
@@ -4,12 +4,13 @@ const { Server } = require("socket.io");
 
 const io = new Server();
 
+// Store active users and their sockets
 let activeUsers = [];
+// Store active group chats
+let groupChats = [];
 
 io.on("connection", (socket) => {
-  // add new user
   socket.on("new-user-add", (newUserId) => {
-    // if user not added previously
     if (!activeUsers.some((user) => user.userId === newUserId)) {
       activeUsers.push({
         userId: newUserId,
@@ -18,28 +19,52 @@ io.on("connection", (socket) => {
     }
     console.log("Connected Users", activeUsers);
 
-    // send all active users to new user
+    // Send all active users to new user
     io.emit("get-users", activeUsers);
   });
 
   socket.on("disconnect", () => {
-    // remove user from active users
     activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
     console.log("User Disconnected", activeUsers);
-
-    // send all active users to all users
     io.emit("get-users", activeUsers);
   });
 
-  // send message to a specific user
   socket.on("send-message", (data) => {
-    const { receiverId } = data;
-    const user = activeUsers.find((user) => user.userId === receiverId);
-    console.log("Sending from socket to :", receiverId);
-    console.log("Data: ", data);
-    if (user) {
-      io.to(user.socketId).emit("recieve-message", data);
+    const { receiverId, chatType, message } = data;
+
+    if (chatType === "user") {
+      const user = activeUsers.find((user) => user.userId === receiverId);
+      if (user) {
+        io.to(user.socketId).emit("receive-message", message);
+      }
+    } else if (chatType === "group") {
+      // Find the group chat by its ID
+      const groupChat = groupChats.find((chat) => chat.id === receiverId);
+      if (groupChat) {
+        groupChat.members.forEach((member) => {
+          const user = activeUsers.find((user) => user.userId === member.userId);
+          if (user) {
+            io.to(user.socketId).emit("receive-group-message", message);
+          }
+        });
+      }
+    }
+  });
+
+  // Create a new group chat
+  socket.on("create-group-chat", (chatData) => {
+    const { chatId, members } = chatData;
+    groupChats.push({ id: chatId, members });
+  });
+
+  // Add a user to an existing group chat
+  socket.on("add-user-to-group-chat", (data) => {
+    const { chatId, userId } = data;
+    const groupChat = groupChats.find((chat) => chat.id === chatId);
+    if (groupChat) {
+      groupChat.members.push({ userId });
     }
   });
 });
+
 module.exports = io;
